c493f56ba0238208a162737bad0c4b37c253eb75
Gilletti Daria <gillettidaria@MacBook-Pro-di-user49.local>
2019-11-22 17:15:49 +0100

    Added new features

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-export function appiddiri() {}
+import { promisify } from 'util';
+import * as fs from 'fs';
+import { userInfo } from 'os';
+
+/*export interface User {
+    nickname: string, 
+    name: string,
+    surname: string,
+    phone: string
+}*/
+
+export const addUser = (nickname: string, name: string, surname: string, phone: string): any  => {
+    let obj = {
+        users: Array<any>()
+    };
+
+    (async () => {
+        try {
+            const readFile = promisify(fs.readFile);
+            const info = await readFile('info.json', 'utf-8');
+            obj = JSON.parse(info);
+            obj.users.push({nickname, name, surname, phone});
+            let json = JSON.stringify(obj);
+            const writeFile = promisify(fs.writeFile);
+            const file = await writeFile('info.json', json, 'utf-8');
+        }
+        catch (err) {
+            console.error(err);
+        }
+    })();
+}
+
+addUser("MainframeTv", "Gabriele", "Connelli", "+393482523775");

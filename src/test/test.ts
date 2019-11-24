e4c492add85def7c6c87cf255ebd8fc8a3a0a280
Gilletti Daria <gillettidaria@MacBook-Pro-di-user49.local>
2019-11-24 16:43:17 +0100

    Added #addChat tests, fixed some errors

--- src/test/test.ts
+++ src/test/test.ts
@@ -4,15 +4,28 @@ import * as methods from '../index';
 const assert = require('assert');
 
 describe('#addUser', () => {
-    it('should return a string if the chat has been added', () => {
-        let result = methods.addUser("MainframeTv", "Gabriele", "Connelli", "+393482523775");
+    let result = methods.addUser("MainframeTv", "Gabriele", "Connelli", "+393482523775");
+    it('should return a string if the user has been added', () => {
         assert.isString(result);
         assert.include(result, 'MainframeTv');
     })
 
+    /*it('should return an error if the user hasn\'t been added', () => {
+        assert.isNotString(result);
+        assert.notInclude(result);
+    })*/
+});
+
+describe('#addChat', () => {
+    it('should return a string if the chat has been added', () => {
+        let result = methods.addChat("Quelli che... si disperano", "Help us, pls", ["+393466457463", "+393286239190", "+393451691678"])
+        assert.isString(result);
+        assert.include(result, 'Quelli che... si disperano');
+    })
+
     /*it('should return an error if the chat hasn\'t been added', () => {
-        assert.isNotString(methods.addUser("MainframeTv", "Gabriele", "Connelli", "+393482523775"));
-        assert.notInclude(methods.addUser("MainframeTv", "Gabriele", "Connelli", "+393482523775"), 'MainframeTv');
+        assert.isNotString(result);
+        assert.notInclude(result);
     })*/
 });
 

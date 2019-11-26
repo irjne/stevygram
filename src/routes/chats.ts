9fdd2d9e01731c8d30d6c739fd4bd5a3a87d77e2
Giusy Giangravè <49571332+GlitchXCIV@users.noreply.github.com>
2019-11-26 10:55:28 +0100

    fixed chats.ts

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -8,46 +8,54 @@ import {
     addChat,
     removeChatById
 } from '../index';
+import { resolveSoa } from 'dns';
 
 const router = express.Router();
 
 //GET - url: /, stampa tutte le chat
 router.get('/', async (req, res) => {
-    getAllChats().then(res => {
-        return res.json(res);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    try{
+        const result = await getAllChats();
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 //- url: /:id/users, stampa tutti gli utenti di una chat;
 router.get('/:id/users', (req, res) => {
     let id = Number(req.params.id);
-    getUsersByChatId(id).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    
+    try {
+        const result = await getUsersByChatId(id);
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 //- url: /:id, stampa tutti i dati di una chat;
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id);
-    getInfoByChatId(id).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    
+    try {
+        const result = await getInfoByChatId(id);
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 // - url: /:id/messages, stampa tutti i messaggi di una chat:
 router.get('/:id/messages', (req, res) => {
     let id = Number(req.params.id);
-    getMessagesByChatId(id).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    
+    try {
+         const result = await getMessagesByChatId(id);
+         res.json(result); 
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 
     //TODO: filter: ?word="pippo", stampa tutti i messaggi contenenti la parola; 
     //TODO: filter: ?user="id", stampa tutti i messaggi di un determinato utente.
@@ -55,41 +63,39 @@ router.get('/:id/messages', (req, res) => {
 
 //PUT - url: /:id + BODY, modifica una chat dando un id.
 router.put('/:id', (req, res) => {
-    let id = Number(req.params.id);
-    let description = req.body.description;
-    let name = req.body.name;
-
-    changeInfoByChatId(id, name, description).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    const {id, description, name} = req.body;
+    
+    try {
+        const result = await changeInfoByChatId(id, name, description);
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 //POST - url: / + BODY, aggiunge una chat.
 router.post('/', (req, res) => {
-    let id = Number(req.body.id);
-    let name = String(req.body.name);
-    let description = String(req.body.description);
-    let users = req.body.users;
-    users = users.split(users, ", ");
-
-    addChat(id, name, description, users).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    const {id, name, description, users} = req.body;
+    const usersArray = users.split(users, ", ");
+    
+    try {
+        const result = await addChat(id, name, description, usersArray);
+        res.json (result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 //DELETE - url: /:id, cancella la chat avendo l'id.
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id);
-
-    removeChatById(id).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    
+    try {
+        const result = await removeChatById(id);
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 export default router; 

da89187aa841a5374f88d88c63a4a5e2ab76e4ed
Landolfo Claudio <landolfoclaudio@MacBook-Pro-di-user52.local>
2019-11-26 10:46:40 +0100

    Fixed users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,49 +5,44 @@ const router = express.Router();
 
 //GET - url: /, stampa tutti gli utenti.
 router.get('/', async (req, res) => {
-    getAllUsers().then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    try{
+        const result = await getAllUsers();
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }
 })
 
 //PUT - url: /:id, modifica un user dando un id + BODY.
 router.put('/:phone', async (req, res) => {
     const { name, phone, surname, nickname } = req.body;
     try {
-        const result = await changeUserByPhone(nickname, name, surname, phone);
+        const result = await changeUserByPhone(nickname, name, surname, phone); 
         res.json(result);
     } catch (err) {
-        return res.status(404).send(`Unexpected error: ${err}`);
+        return res.status(400).send(`Unexpected error: ${err}`);
     }
 })
 
 //POST - url: /, aggiunge un utente nell'app + BODY.
 router.post('/', async (req, res) => {
-    console.log(req.body);
-    let name = String(req.body.name);
-    let surname = String(req.body.surname);
-    let nickname = String(req.body.nickname);
-    let phone = String(req.body.phone);
-
-    addUser(nickname, name, surname, phone).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    const { name, phone, surname, nickname } = req.body;
+    try{
+        const result = await addUser(nickname, name, surname, phone);
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }     
 })
 
 //DELETE - url: /:id, cancella l'utente avendo l'id.
 router.delete(':phone', async (req, res) => {
     let phone = String(req.params.phone);
-    console.log(phone);
-
-    removeUserByPhone(phone).then(result => {
-        return res.json(result);
-    }).catch(err => {
-        return res.status(404).send(`Unexpected error: ${err}`);
-    });
+    try{
+        const result = await removeUserByPhone(phone);
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send(`Unexpected error: ${err}`);
+    }  
 })
-
 export default router; 

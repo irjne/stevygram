9ddb02b90aafa94ece30bc34769f724c41022e9a
Laganà Dario <laganadario@MacBook-Pro-di-user62.local>
2019-11-26 13:11:05 +0100

    Fixed validation properties in users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllUsers, addUser, changeUserByPhone, removeUserByPhone } from '../index';
-
+import { body, param, validationResult } from 'express-validator';
 const router = express.Router();
 
 //GET - url: /, stampa tutti gli utenti.
@@ -14,7 +14,25 @@ router.get('/', async (req, res) => {
 })
 
 //PUT - url: /:id, modifica un user dando un id + BODY.
-router.put('/:phone', async (req, res) => {
+router.put('/:phone', [
+    body('nickname')
+        .isString()
+        .trim(),
+    body('name')
+        .isString()
+        .trim(),
+    body('surname').isString()
+        .trim(),
+    param('phone')
+        .isString()
+        .not().isEmpty()
+        .trim()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
     const { name, phone, surname, nickname } = req.body;
     try {
         const result = await changeUserByPhone(nickname, name, surname, phone); 
@@ -25,7 +43,29 @@ router.put('/:phone', async (req, res) => {
 })
 
 //POST - url: /, aggiunge un utente nell'app + BODY.
-router.post('/', async (req, res) => {
+router.post('/',[
+    body('nickname')
+        .isString()
+        .not().isEmpty()
+        .trim(),
+    body('name')
+        .isString()
+        .not().isEmpty()
+        .trim(),
+    body('surname')
+        .isString()
+        .not().isEmpty()
+        .trim(),
+    body('phone')
+        .isString()
+        .not().isEmpty()
+        .trim()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
     const { name, phone, surname, nickname } = req.body;
     try{
         const result = await addUser(nickname, name, surname, phone);
@@ -36,13 +76,25 @@ router.post('/', async (req, res) => {
 })
 
 //DELETE - url: /:id, cancella l'utente avendo l'id.
-router.delete(':phone', async (req, res) => {
+router.delete('/:phone', [
+    param('phone')
+        .isString()
+        .not().isEmpty()
+        .trim()
+], async (req, res) => {
+    {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() });
+        }
+
     let phone = String(req.params.phone);
-    try{
+    try {
         const result = await removeUserByPhone(phone);
         res.json(result);
     } catch (err) {
         return res.status(400).send(`Unexpected error: ${err}`);
     }  
 })
+
 export default router; 

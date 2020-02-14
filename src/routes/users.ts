c03e6f07ba163047ca160d57dc91504ad8c5327e
erocookv5 <matteodicaragmgmail.com>
2020-02-14 19:03:33 +0100

    cluster 0 online again

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -44,7 +44,7 @@ export const authorization = async (req: any, res: any, next: any) => {
 
 // connection to MongoDB database on cluster
 export const mongoDBConnection = async () => {
-    const host = "mongodb+srv://matteo:stevygram@fakecluster-ahthz.mongodb.net/stevygram0?retryWrites=true&w=majority";
+    const host = "mongodb+srv://matteo:stevygram@cluster0-q7lqh.mongodb.net/stevygram0?retryWrites=true&w=majority";
     mongoose.connect(host, {
         useNewUrlParser: true,
         useUnifiedTopology: true

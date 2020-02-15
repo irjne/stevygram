0a1fc891df7d18de0afd7d7ad9bcfa2d4efd933d
Daria Gilletti <daria.gilletti@gmail.com>
2020-02-15 17:33:05 +0100

    Fixed bugs

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -87,7 +87,7 @@ router.get('/:phone', [
     try {
         mongoDBConnection();
         let phone = req.params.phone;
-        usersModel.find({ phone: phone }, (err: any, user: User) => {
+        usersModel.find({ phone: phone }, (err: any, user: User[]) => {
             if (err) {
                 res.send("Error!");
             } else {
